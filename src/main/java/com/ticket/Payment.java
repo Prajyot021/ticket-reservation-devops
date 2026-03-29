7fc0ce0ecf8c1310c740604ef556126a7703ad45
Prajyot <prajyot.wk01@gmail.com>
2026-03-29 12:14:50 +0530

    change in payment to v.06

--- src/main/java/com/ticket/Payment.java
+++ src/main/java/com/ticket/Payment.java
@@ -11,7 +11,7 @@ public class Payment {
     }
 
     public boolean processPayment(){
-        System.out.println("Payment of " + amount + " processed via " + method + " V.05");
+        System.out.println("Payment of " + amount + " processed via " + method + " V.06");
 
         return true;
 
